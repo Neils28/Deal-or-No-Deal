7dd262ce67f321b85d2909106170d84287f0027b
njh62778 <152446073+njh62778@users.noreply.github.com>
2023-12-07 11:36:13 -0600

    Add files via upload

--- moneyBoard.java
+++ moneyBoard.java
@@ -85,67 +85,111 @@ public class moneyBoard extends HBox {
 		this.getChildren().addAll(remainingVB, group, outVB);
 		
 		SortedLinkedList<Integer> newList = new SortedLinkedList<>();
+		
+		Label OneCent = new Label("1c");
+		OneCent.setPadding(new Insets(0,0,0,50));
+		OneCent.setFont(new Font("Eurostile", 15));	
+		
+		Label One$ = new Label("1");
+		One$.setPadding(new Insets(0,0,0,50));
+		One$.setFont(new Font("Eurostile", 15));	
+		
+		Label Five$ = new Label("5");
+		Five$.setPadding(new Insets(0,0,0,50));
+		Five$.setFont(new Font("Eurostile", 15));	
+		
+		Label Ten$ = new Label("10");
+		Ten$.setPadding(new Insets(0,0,0,50));
+		Ten$.setFont(new Font("Eurostile", 15));	
+		
+		Label TwoFive$ = new Label("25");
+		TwoFive$.setPadding(new Insets(0,0,0,50));
+		TwoFive$.setFont(new Font("Eurostile", 15));
+		
+		Label Fifty$ = new Label("50");
+		Fifty$.setPadding(new Insets(0,0,0,50));
+		Fifty$.setFont(new Font("Eurostile", 15));	
+		
+		Label SevenFive$ = new Label("75");
+		SevenFive$.setPadding(new Insets(0,0,0,50));
+		SevenFive$.setFont(new Font("Eurostile", 15));	
+		
+		Label OneHund = new Label("100");
+		OneHund.setPadding(new Insets(0,0,0,50));
+		OneHund.setFont(new Font("Eurostile", 15));	
+		
+		Label TwoHund = new Label("200");
+		TwoHund.setPadding(new Insets(0,0,0,50));
+		TwoHund.setFont(new Font("Eurostile", 15));	
+		
+		Label ThreeHund = new Label("300");
+		ThreeHund.setPadding(new Insets(0,0,0,50));
+		ThreeHund.setFont(new Font("Eurostile", 15));	
+		
+		Label FourHund = new Label("400");
+		FourHund.setPadding(new Insets(0,0,0,50));
+		FourHund.setFont(new Font("Eurostile", 15));	
+		
+		Label FiveHund = new Label("500");
+		FiveHund.setPadding(new Insets(0,0,0,50));
+		FiveHund.setFont(new Font("Eurostile", 15));	
+		
+		Label SevenFiveHund = new Label("750");
+		SevenFiveHund.setPadding(new Insets(0,0,0,50));
+		SevenFiveHund.setFont(new Font("Eurostile", 15));
+		
 		Label OneThou = new Label("1,000");
-		OneThou.setPadding(new Insets(75, 50, 50, -240));
-		OneThou.setTextFill(Color.BLACK);
+		OneThou.setPadding(new Insets(0,0,0,50));
 		OneThou.setFont(new Font("Eurostile", 15));
 		
 		Label FiveThou = new Label("5,000");
-		FiveThou.setPadding(new Insets(95, 50, 50, -240));
-		FiveThou.setTextFill(Color.BLACK);
+		FiveThou.setPadding(new Insets(0,0,0,50));
 		FiveThou.setFont(new Font("Eurostile", 15));
 		
 		Label TenThou = new Label("10,000");
-		TenThou.setPadding(new Insets(115, 50, 50, -240));
-		TenThou.setTextFill(Color.BLACK);
+		TenThou.setPadding(new Insets(0,0,0,50));
 		TenThou.setFont(new Font("Eurostile", 15));
 		
 		Label TwoFiveThou = new Label("25,000");
-		TwoFiveThou.setPadding(new Insets(135, 50, 50, -240));
-		TwoFiveThou.setTextFill(Color.BLACK);
+		TwoFiveThou.setPadding(new Insets(0,0,0,50));
 		TwoFiveThou.setFont(new Font("Eurostile", 15));
 		
 		Label FiftyThou = new Label("50,000");
-		FiftyThou.setPadding(new Insets(155, 50, 50, -240));
-		FiftyThou.setTextFill(Color.BLACK);
+		FiftyThou.setPadding(new Insets(0,0,0,50));
 		FiftyThou.setFont(new Font("Eurostile", 15));
 
 		Label OneHundThou = new Label("100,000");
-		OneHundThou.setPadding(new Insets(175, 50, 50, -240));
-		OneHundThou.setTextFill(Color.BLACK);
+		OneHundThou.setPadding(new Insets(0,0,0,50));
 		OneHundThou.setFont(new Font("Eurostile", 15));
 
 		Label TwoHundThou = new Label("200,000");		
-		TwoHundThou.setPadding(new Insets(195, 50, 50, -240));
-		TwoHundThou.setTextFill(Color.BLACK);
+		TwoHundThou.setPadding(new Insets(0,0,0,50));
 		TwoHundThou.setFont(new Font("Eurostile", 15));
 
 		Label ThreeHundThou = new Label("300,000");
-		ThreeHundThou.setPadding(new Insets(215, 50, 50, -240));
-		ThreeHundThou.setTextFill(Color.BLACK);
+		ThreeHundThou.setPadding(new Insets(0,0,0,50));
 		ThreeHundThou.setFont(new Font("Eurostile", 15));
 
 		Label FourHundThou = new Label("400,000");
-		FourHundThou.setPadding(new Insets(235, 50, 50, -240));
-		 FourHundThou.setTextFill(Color.BLACK);
-		 FourHundThou.setFont(new Font("Eurostile", 15));
+		FourHundThou.setPadding(new Insets(0,0,0,50));
+		FourHundThou.setFont(new Font("Eurostile", 15));
 
 		Label FiveHundThou = new Label("500,000");
-		FiveHundThou.setPadding(new Insets(255, 50, 50, -240));
-		FiveHundThou.setTextFill(Color.BLACK);
+		FiveHundThou.setPadding(new Insets(0,0,0,50));
 		FiveHundThou.setFont(new Font("Eurostile", 15));
 
+		Label SevenFiveThou = new Label("750,000");
+		SevenFiveThou.setPadding(new Insets(0,0,0,50));
+		SevenFiveThou.setFont(new Font("Eurostile", 15));
+
 		Label OneMil = new Label("1,000,000");
-		OneMil.setPadding(new Insets(275, 50, 50, -240));
-		OneMil.setTextFill(Color.BLACK);
+		OneMil.setPadding(new Insets(0,0,0,50));
 		OneMil.setFont(new Font("Eurostile", 15));
+		
 
-		Label OnePointFiveMil = new Label("1,500,000");
-		OnePointFiveMil.setPadding(new Insets(295, 50, 50, -240));
-		OnePointFiveMil.setTextFill(Color.BLACK);
-		OnePointFiveMil.setFont(new Font("Eurostile", 15));
-
-		this.getChildren().addAll(OneThou,FiveThou,TenThou,TwoFiveThou,FiftyThou,OneHundThou,TwoHundThou,ThreeHundThou,FourHundThou,FiveHundThou,OneMil,OnePointFiveMil);
+		remainingVB.getChildren().addAll(OneCent,One$,Five$,Ten$,TwoFive$,Fifty$,SevenFive$,OneHund,TwoHund,ThreeHund,FourHund,
+				FiveHund,SevenFiveHund,OneThou,FiveThou,TenThou,TwoFiveThou,FiftyThou,OneHundThou,TwoHundThou,ThreeHundThou,
+				FourHundThou,FiveHundThou,SevenFiveThou,OneMil);
 		
 	}
 		}
