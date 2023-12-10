767dcf043b18d62dd3ac4cf1ccb57776e79f5a81
NashKraemer <152446291+NashKraemer@users.noreply.github.com>
2023-12-10 17:09:13 -0600

    Add files via upload

--- DealOrNoDealApp.java
+++ DealOrNoDealApp.java
@@ -1,78 +1,87 @@
-package dealorNoDeal;
+package deal.Game;
 
 import java.util.ArrayList;
 
 //TODO 
-// -Create the constructor that talks with StairPane so that everything is displayed in StairsPane
-// -Create the end of the game logic 
+//-Create the constructor that talks with StairPane so that everything is displayed in StairsPane
+//-Create the end of the game logic 
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 
 public class DealOrNoDealApp {
+
+	// Current number of rounds
+	private int[] currentRoundNum;
 	
-	//Created array of the buttons
+	// Created array of the buttons
 	private Button caseButton;
-	
-	//Button to take Deal from Banker
+
+	// Button to take Deal from Banker
 	private Bank Deal;
-			
-	//This is instance of the PriorityQueueLogic class
-	PriorityQueueLogic priorityQueueInstance = new PriorityQueueLogic();
+
+//	// This is instance of the PriorityQueueLogic class
+//	PriorityQueueLogic priorityQueueInstance = new PriorityQueueLogic();
 
 	// Button that sends cases to the Queue
-	private Button casesConfirmationButton; 
-
-	//Create an array of the caseNumbers that are chosen
+//	private Button casesConfirmationButton;
+//
+//	public Button getConfirmBT() {
+//		return casesConfirmationButton;
+//	}
+	
+	private GUI[] temp;
+	// Create an array of the caseNumbers that are chosen
 	ArrayList<Integer> caseNumbers = new ArrayList<Integer>();
 
 	Case cases = new Case();
-	StairsPane stairsPane = new StairsPane(cases, null);
 
-	public DealOrNoDealApp(HBox stair, int[] casesArrayT, int start, int end){
+	public DealOrNoDealApp(HBox stair, int[] casesArrayT, int start, int end, int RNum, GUI gui) {
+		temp = new GUI[1];
+		temp[0] = gui;
 		for (int i = start; i < end; i++) {
+			currentRoundNum = new int[1];
+			currentRoundNum[0]= RNum;
 			Button caseButton = new Button(Integer.toString(casesArrayT[i]));
 			// Customize the appearance of the button to look like a briefcase
 			caseButton.setStyle("-fx-background-color: #D3D3D3; -fx-font-size: 18;");
 			// Add an event handler to the button
-			
-	        // Pass the value of the button to the event handler, so th
-            caseButton.setOnAction(event -> handleButtonClicked(event, caseNumbers));
-            
+
+			// Pass the value of the button to the event handler, so th
+			caseButton.setOnAction(event -> handleButtonClicked(event, caseNumbers));
+
 			stair.getChildren().add(caseButton);
 		}
 	}
-	
-	
+
 	public void handleButtonClicked(ActionEvent event, ArrayList<Integer> caseNumbers) {
-		//Set the Current Round Number
-		int currentRoundNum = 1;
-		
+
+
 		// This is for Round 1 and 8: where only one case is chosen
-		if (currentRoundNum == 1 || currentRoundNum == 8) {
+		if (currentRoundNum[0] == 1 || currentRoundNum[0] == 8) {
 			for (int chosenNumCases = 0; chosenNumCases == 1; chosenNumCases++) {
 				if (event.getSource() == caseButton) {
 					chosenNumCases++; // Number of cases chosen increases
-					casesConfirmationButton.setVisible(true);
-					currentRoundNum++;
+					temp[0].getSP().getConfirmBT().setVisible(true);
+					currentRoundNum[0]++;
 				}
-				if (event.getSource() == casesConfirmationButton) {
-					Deal.setVisible(true); //Changes the Deal button to visible
-			        priorityQueueInstance.addChosenCase(caseNumbers); // Send the caseNumber to PriorityQueueLogic
-				}
+//				if (event.getSource() == casesConfirmationButton) {
+//					Deal.setVisible(true); // Changes the Deal button to visible
+//					priorityQueueInstance.addChosenCase(caseNumbers); // Send the caseNumber to PriorityQueueLogic
+//				}
 			}
 		}
 
 		// This is for Round 2-7: where four cases are chosen and sent to the Queue
-		if (currentRoundNum > 1 || currentRoundNum < 8) {
+		if (currentRoundNum[0] > 1 || currentRoundNum[0] < 8) {
 			for (int chosenNumCases = 0; chosenNumCases == 4; chosenNumCases++) {
 				if (event.getSource() == caseButton) {
 					chosenNumCases++; // Number of cases chosen increases
 				}
 				if (chosenNumCases == 4) {
-					currentRoundNum++; // Increase the current Round 1
-					casesConfirmationButton.setVisible(true);
+					currentRoundNum[0]++; // Increase the current Round 1
+					temp[0].getSP().getConfirmBT().setVisible(true);
 				}
 				if (event.getSource() == casesConfirmationButton) {
 					priorityQueueInstance.addChosenCase(caseNumbers); // Send the caseNumber to PriorityQueueLogic
@@ -80,10 +89,10 @@ public class DealOrNoDealApp {
 			}
 		}
 		// This is the end of the Game
-		if (event.getSource() == Deal || currentRoundNum > 8) {
+		if (event.getSource() == Deal || currentRoundNum[0] > 8) {
 			System.out.println("Congratulations. Game over brother");
-			// TODO Change this GUI later, this pops up when the deal button is clicked that ends the game
+			// TODO Change this GUI later, this pops up when the deal button is clicked that
+			// ends the game
 		}
-
 	}
 }
--- StairsPane.java
+++ StairsPane.java
@@ -1,22 +1,41 @@
 package deal.Game;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
-public class StairsPane extends VBox {
-	//Set Value for the 
+public class StairsPane extends VBox implements EventHandler<ActionEvent> {
+	// Set Value for the
 	private static final int BOTTOM_INVALID = 0;
-	
-	//Instance of the DealOrNoDealApp Class
-	private DealOrNoDealApp dealOND;
-	
-	//Constructor for the StairsPane Class
+
+	private int numrounds = 1;
+
+	private Button casesConfirmationButton;
+
+	private GUI[] temp;
+
+	public Button getConfirmBT() {
+		return casesConfirmationButton;
+	}
+
+	// This is instance of the PriorityQueueLogic class
+	PriorityQueueLogic priorityQueueInstance = new PriorityQueueLogic();
+
+	// Instance of the DealOrNoDealApp Class
+	// private DealOrNoDealApp dealOND;
+
+	// Constructor for the StairsPane Class
 //    public StairsPane(Case cases) {
 //        this.dealOND = cases;
 //        cases.populateStairs(null, null, BOTTOM_INVALID, BOTTOM_INVALID);
 //    }
-	
-	public StairsPane(Case cases, DealOrNoDealApp dealOND) {
+
+	public StairsPane(Case cases, GUI gui) {
+		temp = new GUI[1];
+		temp[0] = gui;
 
 		this.setSpacing(100);
 		HBox stair1HB = new HBox(70);
@@ -25,12 +44,34 @@ public class StairsPane extends VBox {
 		HBox stair3HB = new HBox(70);
 		HBox stair4HB = new HBox(70);
 
-		dealOND.populateStairs(stair1HB, cases.getCasesArray(), 0, 8);
-		dealOND.populateStairs(stair2HB, cases.getCasesArray(), 8, 16);
-		dealOND.populateStairs(stair3HB, cases.getCasesArray(), 16, 24);
-		dealOND.populateStairs(stair4HB, cases.getCasesArray(), 24, cases.getCasesArray().length);
+		casesConfirmationButton = new Button("Conform Cases");
+		casesConfirmationButton.setVisible(false);
+		casesConfirmationButton.setOnAction(this);
 
-		getChildren().addAll(stair1HB, stair2HB, stair3HB, stair4HB);
-		
+		DealOrNoDealApp DND1 = new DealOrNoDealApp(stair1HB, cases.getCasesArray(), 0, 8, numrounds, gui);
+		DealOrNoDealApp DND2 = new DealOrNoDealApp(stair2HB, cases.getCasesArray(), 8, 16, numrounds, gui);
+		DealOrNoDealApp DND3 = new DealOrNoDealApp(stair3HB, cases.getCasesArray(), 16, 24, numrounds, gui);
+		DealOrNoDealApp DND4 = new DealOrNoDealApp(stair4HB, cases.getCasesArray(), 24, cases.getCasesArray().length,
+				numrounds, gui);
+
+//		dealOND.populateStairs(stair1HB, cases.getCasesArray(), 0, 8);
+//		dealOND.populateStairs(stair2HB, cases.getCasesArray(), 8, 16);
+//		dealOND.populateStairs(stair3HB, cases.getCasesArray(), 16, 24);
+//		dealOND.populateStairs(stair4HB, cases.getCasesArray(), 24, cases.getCasesArray().length);
+
+		getChildren().addAll(stair1HB, stair2HB, stair3HB, stair4HB, casesConfirmationButton);
+
 	}
+
+	@Override
+	public void handle(ActionEvent event) {
+		if (numrounds == 1 || numrounds == 8) {
+
+			if (event.getSource() == casesConfirmationButton) {
+				Deal.setVisible(true); // Changes the Deal button to visible
+				priorityQueueInstance.addChosenCase(caseNumbers); // Send the caseNumber to PriorityQueueLogic
+			}
+		}
+	}
+
 }
