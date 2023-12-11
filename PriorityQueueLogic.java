6c5526339127729cb87cded55cd1c1c3aadd50ea
Neils28 <106360853+Neils28@users.noreply.github.com>
2023-12-10 19:52:45 -0600

    Add files via upload

--- PriorityQueueLogic.java
+++ PriorityQueueLogic.java
@@ -1,39 +1,50 @@
 package dealorNoDeal;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.PriorityQueue;
 
 public class PriorityQueueLogic extends Case {
-	//Instance of the Class Cases
+	// Instance of the Class Cases
 	Case casesInstance = new Case();
-	
-	//Instance of the Array within the Cases Class
+
+	// Instance of the Array within the Cases Class
 	int[] casesArrayTotal = casesInstance.casesArray;
-	
-	//Priority Queue to store and pop off integers from Cases
+
+	// Priority Queue to store and pop off integers from Cases
 	PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
-	
-	//Case that is popped off the PriorityQueue and then sent to the LinkedList
-	int poppedCase;
-	
+
+	// Case that is popped off the PriorityQueue and then sent to the LinkedList
+	private Integer poppedCase;
+
 	// Display the numbers that popped off the PriorityQueue within the LinkedList
 	SortedLinkedList<Integer> CasesLL = new SortedLinkedList<>();
-	//TODO Replace this with Nathan's LinkedList
-	
-	//Constructor for the Priority Queue 
+	// TODO Replace this with Nathan's LinkedList
+
+	// This is the Users case, the first case on the Queue that isn't removed
+	private Integer firstCase;
+
+	// Constructor for the Priority Queue
 	public PriorityQueueLogic() {
 		this.priorityQueue = new PriorityQueue<>();
+		this.firstCase = null;
 	}
-	
-	//Method that takes in the number from of the Case and puts into the Queue 
+
+	// Method that takes in the number from of the Case and puts into the Queue
 	public void addChosenCase(ArrayList<Integer> caseNumbers) {
+		// Checks to make sure you do not reset the first case every time method
+		// restarts
+		// Also checks to make sure that the array of cases is zero before adding the first case
+		if (firstCase == null && !caseNumbers.isEmpty()) {
+			firstCase = caseNumbers.get(0);
+			caseNumbers.remove(0);
+		}
+		
 		// Add the chosen case number to the priority queue
 		priorityQueue.addAll(caseNumbers);
-		
-		//Cases are Taken off the Priority Queue and then put into the LinkedList
-		if(priorityQueue.size() > 1) {
-			int poppedCase = priorityQueue.poll();
+
+		// Cases are Taken off the Priority Queue and then put into the LinkedList
+		if (priorityQueue.size() > 1) {
+			poppedCase = priorityQueue.poll();
 			CasesLL.addItem(poppedCase);
 		}
 	}
